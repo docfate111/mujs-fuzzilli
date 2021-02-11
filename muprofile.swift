5cb89055aa802fa4d259f1ee729c740289b7d174
docfate111 <tdwilliamsiv@gmail.com>
2021-02-10 21:51:15 -0800

    profile for older release

--- muprofile.swift
+++ muprofile.swift
@@ -32,11 +32,12 @@ let muProfile = Profile(
 
     crashTests: ["crash();"],
 
-    additionalCodeGenerators: WeightedList<CodeGenerator>([
-        (ForceV8TurbofanGenerator, 10),
-    ]),
+    // additionalCodeGenerators: WeightedList<CodeGenerator>([
+    //     (ForceV8TurbofanGenerator, 10),
+    // ]),
 
-    builtins: defaultBuiltins + ["gc", "BigInt", "BigUint64Array", "BigInt64Array", "SharedArrayBuffer", "Atomics"],
+    builtins: defaultBuiltins,
+    // + ["gc", "BigInt", "BigUint64Array", "BigInt64Array", "SharedArrayBuffer", "Atomics"],
     propertyNames: defaultPropertyNames,
     methodNames: defaultMethodNames
 )
